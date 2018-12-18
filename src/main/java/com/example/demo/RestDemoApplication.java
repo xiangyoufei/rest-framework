079e80489a6decd2e4c2b34c4924932224a8d776
lysss <liuyang.sx@qq.com>
2018-12-18 08:28:59 +0800

    添加了spring-boot 缓存配置 但是具体代码实现还没测试

--- src/main/java/com/example/demo/RestDemoApplication.java
+++ src/main/java/com/example/demo/RestDemoApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages="com.example.demo")
 @ServletComponentScan
 @MapperScan("com.example.demo.dao")//类文件的位置
 @EnableTransactionManagement//开启mybatis事务
+@EnableCaching//自动化配置合适的缓存管理器（CacheManager）
 public class RestDemoApplication {
 
 	public static void main(String[] args) {
